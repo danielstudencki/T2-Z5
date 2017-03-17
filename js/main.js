bfddd71170de74f5862fd2253674118c88655cbf
Daniel Studencki <denyllon@gmail.com>
2017-03-17 01:59:34 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var elem = new Toggler("#section");
+var button = document.querySelector("#button");
+ 
+button.addEventListener("click", function() {
+ 
+    if(elem.getElem().style.display == "none") {
+        elem.show();
+    } else {
+        elem.hide();
+    }
+ 
+}, false);
